99785156d78b1d3bc3ecbca3d4606afb0dadfe33
Hannah <hannahwoodward@hotmail.com>
2022-04-14 15:22:56 +0800

    saved changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,17 +13,17 @@ class PostsController < ApplicationController
 
 	# app/controllers/posts_controller.rb
 
-def create
+	def create
 	@post = Post.new(params.require(:post).permit(:title, :description))
 	@post.save
 	redirect_to post_path(@post)
-  end
-  
-  def update
+	end
+
+	def update
 	@post = Post.find(params[:id])
 	@post.update(params.require(:post).permit(:title))
 	redirect_to post_path(@post)
-  end
+	end
 
 	def edit
 	  @post = Post.find(params[:id])
